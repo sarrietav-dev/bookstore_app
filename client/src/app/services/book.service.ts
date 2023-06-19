9d9ca2d3bd9504deb2e1d45e8c5f667b212ae633
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-06-18 19:50:43 -0500

    Create book service

--- client/src/app/services/book.service.ts
+++ client/src/app/services/book.service.ts
@@ -1,9 +1,39 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+
+type Book = {
+  id: number;
+  title: string;
+  category: string;
+};
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BookService {
+  private URL = `${environment.apiUrl}/books`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(this.URL);
+  }
+
+  getBook(id: number): Observable<Book> {
+    return this.http.get<Book>(`${this.URL}/${id}`);
+  }
+
+  createBook(book: Book): Observable<Book> {
+    return this.http.post<Book>(this.URL, book);
+  }
+
+  updateBook(book: Book): Observable<Book> {
+    return this.http.put<Book>(`${this.URL}/${book.id}`, book);
+  }
+
+  deleteBook(id: number): Observable<Book> {
+    return this.http.delete<Book>(`${this.URL}/${id}`);
+  }
 }
